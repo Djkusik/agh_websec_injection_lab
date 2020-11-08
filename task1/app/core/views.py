5986f28d055301ef2b160d171075582ab5727245
Paweł Kusiński <KusinskiP@protonmail.com>
2020-11-08 01:51:51 +0100

    Update task1 vuln

--- task1/app/core/views.py
+++ task1/app/core/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for, \
                   abort, jsonify
-from app import gryphon_bot
+from app import gryphon_bot, TASK_PATH, app
 
 
 mod = Blueprint('core', __name__)
@@ -16,10 +16,11 @@ def gryphon():
     return str(gryphon_bot.get_response(answer))
 
 
-@mod.route('/task', methods=['GET', 'POST'])
+@mod.route(TASK_PATH, methods=['GET'])
 def task():
-    if request.method == 'GET':
+    name = request.args.get('name')
+    if name is not None:
+        template = app.jinja_env.from_string(name).render()
+        return (render_template('core/task.html', name=template))
+    else:
         return (render_template('core/task.html'))
-    if request.method == 'POST':
-        answer = request.form.get('answer')
-        return (answer)
